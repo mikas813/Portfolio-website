906f25fca9413e98148fd01c25c745da3279a993
Mykhailo Lasiichuk <mishalasiychyk@MacBook-Air-Misha.local>
2020-11-24 17:39:01 +0000

    err msg

--- src/components/Cube/ContactSide/Contacts.jsx
+++ src/components/Cube/ContactSide/Contacts.jsx
@@ -6,23 +6,24 @@ import emailjs from 'emailjs-com';
 export const ContactSide = () => {
 
     const sendEmail = (e) => {
+
         e.preventDefault();
-        document.getElementById('box').classList.value = '';
-        document.getElementById('box').classList.add('rotate');
+
+        const box = document.getElementById('box');
+
+        box.classList.value = '';
+        box.classList.add('rotate');
+
         emailjs.sendForm(
             'service_yx4jqaf',
             'template_1u82alm',
             e.target,
             'user_3hIFmLZWQw8fFEPF6xpY0').then(() => {
-            setTimeout(() => {
                 type(content.message, 'message-side');
-            }, 5000);
-            document.querySelector('form').reset();
         }, () => {
-            setTimeout(() => {
                 type(content.error, 'message-side');
-            }, 5000);
         });
+        document.querySelector('form').reset();
     };
 
     return (

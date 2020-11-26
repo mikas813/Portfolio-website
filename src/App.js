475dc1e371bc73f4ed25ca522f2ae7b05b77f4be
Mykhailo Lasiichuk <mishalasiychyk@MacBook-Air-Misha.local>
2020-11-26 16:02:57 +0000

    ga

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import {SocLinks} from './components/SocLinks/SocLinks';
 import {Border} from './components/Border/Border';
 import {Content} from './components/Content';
 import {Cube} from './components/Cube/Cube';
+import ReactGA from 'react-ga';
+import React, {useEffect} from 'react';
 
 export const App = () => {
+
+    useEffect(() => {
+        ReactGA.initialize('G-03EE6RX9B4');
+        ReactGA.pageview('/')
+        }, []);
+
     return (
         <div className="App">
-                <Border>
-                    <Header/>
-                    <Navigation/>
-                        <Content>
-                            <Cube/>
-                        </Content>
-                    <SocLinks/>
-                    <Footer/>
-                </Border>
+            <Border>
+                <Header/>
+                <Navigation/>
+                <Content>
+                    <Cube/>
+                </Content>
+                <SocLinks/>
+                <Footer/>
+            </Border>
         </div>
     );
 };
